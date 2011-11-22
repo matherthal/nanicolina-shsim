bd83917a5674cc5305e788a104951bb820717e2c
Douglas Mareli <douglasropb@gmail.com>
2011-11-22 16:40:16 -0200

    Implementa??o de Local e Position

--- Nanicolina-shsim/src/nanicolinashsim/RegistryService.java
+++ Nanicolina-shsim/src/nanicolinashsim/RegistryService.java
@@ -1,28 +1,41 @@
 package nanicolinashsim;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.4882BF1A-C211-E60D-5C0F-58091CEBCBA6]
 // </editor-fold> 
 public class RegistryService extends ResourceAgent {
 
+    private List<ResourceAgent> resources;
+
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.FEA29F7B-537E-AE0D-9AA8-13CEBA03E58E]
     // </editor-fold> 
-    public RegistryService () {
+    public RegistryService (String URN, String URL) {
+        super.setURN(URL);
+        super.setURL(URL);
+        resources = new ArrayList();
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.09D4FE71-B5E2-C0A9-AF34-B54506BA4534]
     // </editor-fold> 
-    public int register (ResourceAgent resource) {
-        return 0;
+    public boolean register (ResourceAgent resource)
+    {
+        resources.add(resource);
+        return true;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D3BBCD65-FA68-5B5C-0930-DC8AA0CE41DE]
     // </editor-fold> 
-    public void unregister (ResourceAgent resource) {
+    public boolean unregister (ResourceAgent resource)
+    {
+        resources.remove(resource);
+        return true;
     }
 
 }
