8699c9c40a150f3c980cba7bf52b1fe4ae3036a7
Douglas Mareli <douglasropb@gmail.com>
2011-11-22 17:50:14 -0200

    adding entity

---
+++ Nanicolina-shsim/src/nanicolinashsim/ResourceRepository.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nanicolinashsim;
+
+import java.util.List;
+
+/**
+ *
+ * @author Mareli
+ */
+public class ResourceRepository {
+
+    private static ResourceRepository instance = null;
+
+    private ResourceRepository()
+    {
+
+    }
+    public static ResourceRepository getInstance()
+    {
+        if (instance == null)
+            instance = new ResourceRepository();
+        return instance;
+    }
+
+    public List<ResourceAgent> resources;
+
+}
