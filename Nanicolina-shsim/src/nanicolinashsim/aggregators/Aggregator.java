f232d5fb5c4601e62f7e9ff043de4cf8060da0b0
matherthal <matherthal@localhost>
2011-12-01 13:07:42 -0200

    adding the aggregators, rules, and widgets that were missing

---
+++ Nanicolina-shsim/src/nanicolinashsim/aggregators/Aggregator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nanicolinashsim.aggregators;
+
+/**
+ *
+ * @author matheus
+ */
+public interface Aggregator {
+
+}
