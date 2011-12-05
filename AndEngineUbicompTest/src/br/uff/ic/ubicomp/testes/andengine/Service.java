4925fb70859da0d8ed1c0e82362062afdcdb83a4
David <David@localhost>
2011-12-05 20:17:39 -0200

    iaohsdoiashio

--- AndEngineUbicompTest/src/br/uff/ic/ubicomp/testes/andengine/Service.java
+++
@@ -1,8 +0,0 @@
-package br.uff.ic.ubicomp.testes.andengine;
-
-import java.util.ArrayList;
-
-public class Service {
-	//public String name;
-	//public ArrayList<ServiceProperty> properties = new ArrayList<ServiceProperty>();	
-}
