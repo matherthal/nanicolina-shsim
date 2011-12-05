2af3b7a2819b3a962a56fe9b8c5d3f0a99390a23
David <David@localhost>
2011-12-05 20:17:39 -0200

    iaohsdoiashio

--- AndEngineUbicompTest/src/br/uff/ic/ubicomp/testes/andengine/ResourceProperty.java
+++
@@ -1,6 +0,0 @@
-package br.uff.ic.ubicomp.testes.andengine;
-
-public class ResourceProperty {
-	public String name;
-	public String state;
-}
--- AndEngineUbicompTest/src/br/uff/ic/ubicomp/testes/andengine/ServiceProperty.java
+++
@@ -1,6 +0,0 @@
-package br.uff.ic.ubicomp.testes.andengine;
-
-public class ServiceProperty {
-	public String name;
-	public String state;
-}
