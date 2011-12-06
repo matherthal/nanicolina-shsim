61f749ed0566c0ec67f399b0eefb055446785ba7
david.brto <david.brto@localhost>
2011-12-06 11:34:09 -0200

    tentativa de acerta o projeto

---
+++ AndEngineUbicompTest/src/br/uff/ic/ubicomp/testes/knowledge/Refrigerator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.uff.ic.ubicomp.testes.knowledge;
+
+/**
+ *
+ * @author dbarreto
+ */
+public class Refrigerator extends Widget {
+
+    public Refrigerator(String urn, String url, Position pos) {
+        super(urn, url, pos);
+    }
+
+}
---
+++ AndEngineUbicompTest/src/nanicolinashsim/widgets/Bed.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nanicolinashsim.widgets;
+
+import br.uff.ic.ubicomp.testes.knowledge.Position;
+import br.uff.ic.ubicomp.testes.knowledge.Widget;
+
+//import nanicolinashsim.Widget;
+//import nanicolinashsim.Position;
+
+/**
+ *
+ * @author matheus
+ */
+public class Bed extends Widget {
+
+    private boolean used = false;
+
+    public Bed(String urn, String url, Position pos) {
+        super(urn, url, pos);
+    }
+
+    public boolean getUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+}
